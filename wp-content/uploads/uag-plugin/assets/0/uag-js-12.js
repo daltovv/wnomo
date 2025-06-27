86112e06841177b4b8d2d2fa7349ddd95f4e5d84
daltov <guaman.dalto.bvm@gmail.com>
2025-06-27 03:45:15 -0500

    Subida de archivos actualizacion

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-12.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-b10e8788' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-7451f4b2' );
+});
+ });
