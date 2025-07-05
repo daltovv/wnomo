b60b3ef2b559b24bf841635910877bad160249be
daltov <guaman.dalto.bvm@gmail.com>
2025-07-05 04:00:59 -0500

    Subida de archivos actualizacion 1

--- wp-content/uploads/uag-plugin/assets/0/uag-js-12.js
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-12.js
@@ -1,7 +1,4 @@
 document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-b10e8788' );
-});
-window.addEventListener( 'load', function() {
-	UAGBButtonChild.init( '.uagb-block-7451f4b2' );
+	UAGBButtonChild.init( '.uagb-block-6a589c12' );
 });
  });
